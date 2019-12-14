2a69d52d93beb0058ab2ce0175b6d50134276753
Mucsi Gergely <gergely22mucsi@gmail.com>
2019-12-14 17:20:14 +0100

    API, REST framework, API testing program added

--- toowtrsywen/check_in_out/urls.py
+++ toowtrsywen/check_in_out/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.urls import include, path
 from . import views
+from rest_framework import routers
 
 app_name = "check_in_out"
+router = routers.DefaultRouter()
+router.register('check_in_out',views.CheckTimeView)
 
 urlpatterns = [
-    path('check_in_out/', views.main_text, name="main_text"),
+    # path('check_in_out/', views.main_text, include(router.urls),name="main_text"),
+    path('check_in_out/', include(router.urls)),
 
 ]
--- toowtrsywen/check_in_out/views.py
+++ toowtrsywen/check_in_out/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import CheckTime
+from .serializers import CheckTimeSerializer
+from rest_framework import viewsets
 # Create your views here.
+class CheckTimeView(viewsets.ModelViewSet):
+    queryset = CheckTime.objects.all()
+    #queryset = CheckTime.objects.create(name_text='testjani',in_or_out='in')
+    serializer_class = CheckTimeSerializer
+
 def main_text(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # CheckTime.objects.create(name_text='testjani',in_or_out='in')
+    return HttpResponse("Added.")
