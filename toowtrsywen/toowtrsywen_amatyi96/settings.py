23a572c03ca20b2a8ddf1fa974d29e1e6b88266b
Mucsi Gergely <gergely22mucsi@gmail.com>
2019-12-14 04:02:46 +0100

    checkin, checkout started

--- toowtrsywen/toowtrsywen_amatyi96/settings.py
+++ toowtrsywen/toowtrsywen_amatyi96/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'check_in_out.apps.CheckInOutConfig',
 ]
 
 MIDDLEWARE = [
